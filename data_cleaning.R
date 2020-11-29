b6fec79e9c3bb5b0eb313b1d9dd128dc61e435de
Rafael Hiroshi <rafaelhkp@gmail.com>
2020-11-29 12:26:31 -0300

    Remove whitespace at the leading and trailing ends

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,8 +1,3 @@
-library(plyr)
-#install.packages('plyr')
-
-View(rdo_database)
-
 rdo_database <- read.csv('RDO_3.csv')
 rdo_database["ID_DELEGACIA"] = rdo_database[colnames(rdo_database)[1]]
 rdo_database = rdo_database[!duplicated(rdo_database[c("NUM_BO", "ANO_BO", "ID_DELEGACIA")]), ]
@@ -32,19 +27,19 @@ rdo_database$RUBRICA[rdo_database$RUBRICA == "Lesão corporal  de natureza 'GRAV
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Homicídio qualificado (art. 121, §2o.)'] <- 'Homicídio qualificado'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'Homicídio culposo (art. 121, §3o.)'] <- 'Homicídio culposo'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Lesão corporal (art 129 § 9º)'] <- 'Lesão corporal'
-rdo_database$RUBRICA[rdo_database$RUBRICA == 'Furto de coisa comum (art. 156)'] <- 'Furto'
+rdo_database$RUBRICA[rdo_database$RUBRICA == 'Furto de coisa comum (art. 156)'] <- 'Furto de coisa comum'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Estupro (art.213)'] <- 'Estupro'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Lesão corporal culposa (art. 129. §6o.)'] <- 'Lesão corporal culposa'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'Lesão corporal seguida de morte (art. 129, §3o.)'] <- 'Lesão corporal seguida de morte'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Homicídio simples (art. 121)'] <- 'Homicídio simples'
-rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Furto de coisa comum (art. 156)'] <- 'Furto'
+rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Furto de coisa comum (art. 156)'] <- 'Furto de coisa comum'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Homicídio culposo na direção de veículo automotor (Art. 302)'] <- 'Homicídio culposo na direção de veículo automotor'
 rdo_database$RUBRICA[rdo_database$RUBRICA == 'A.I.-Lesão corporal de natureza GRAVE (art. 129, §1o.)'] <- 'Lesão corporal de natureza GRAVE'
 rdo_database$RUBRICA <- as.factor(rdo_database$RUBRICA)
 
-cidades <- sort(table(rdo_database["CIDADE"]), decreasing=TRUE)
-saoPaulo <- rownames(cidades)[1]
-rdo_database <- rdo_database[rdo_database$CIDADE == saoPaulo,]
+cols_to_be_rectified <- names(rdo_database)[vapply(rdo_database, is.factor, logical(1))]
+rdo_database[,cols_to_be_rectified] <- lapply(rdo_database[,cols_to_be_rectified], trimws)
+rdo_database <- rdo_database[rdo_database$CIDADE == "S.PAULO",]
 
 rdo_database$HORA <- strtoi(substr(rdo_database[['HORA_OCORRENCIA_BO']],0, 2), base=10L)
 
@@ -59,6 +54,19 @@ rdo_database$PERIODO_DESCRICAO[rdo_database$HORA >= 12 & rdo_database$HORA < 18]
 rdo_database$PERIODO_DESCRICAO[rdo_database$HORA >= 18 & rdo_database$HORA < 24] <-"Noturno"
 rdo_database$PERIODO_DESCRICAO[rdo_database$HORA >= 0 & rdo_database$HORA < 6] <-"Madrugada"
 rdo_database$PERIODO[is.na(rdo_database$HORA)] <-"Indefinido"
+
+rdo_database$COR_CUTIS[
+  rdo_database$COR_CUTIS != 'Amarela' &
+  rdo_database$COR_CUTIS != 'Branca' &
+  rdo_database$COR_CUTIS != 'NULL' & 
+  rdo_database$COR_CUTIS != 'Outros' & 
+  rdo_database$COR_CUTIS != 'Parda' & 
+  rdo_database$COR_CUTIS != 'Preta' & 
+  rdo_database$COR_CUTIS != 'Vermelha'] <- NA
+
+rdo_database$COR_CUTIS[rdo_database$COR_CUTIS == 'NULL'] <- NA
+
+rdo_database$SEXO_PESSOA[rdo_database$SEXO_PESSOA == 'NULL'] <- NA
 
 write.csv(rdo_database,"RDO_3_v2.csv", row.names = FALSE)
 
